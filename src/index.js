86564a7256975716a4db0b8fc4976455cce7df9e
Stephanie Mathew <stephanie.mathew@liongard.com>
2020-11-18 09:31:35 -0600

    Create CardList component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './components/Card';
+import CardList from './components/CardList';
 import { robots } from './robots';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* // Create greeting prop */}
-    <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-    </div>
+    <CardList robots={robots}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
