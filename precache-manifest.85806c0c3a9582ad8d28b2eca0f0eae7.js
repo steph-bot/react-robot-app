3dd097e8624f662f378172c5de669d6b9e91724f
Stephanie Mathew <stephanie.mathew@liongard.com>
2021-01-07 17:33:11 -0600

    Updates

---
+++ precache-manifest.85806c0c3a9582ad8d28b2eca0f0eae7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5718bd4198ec910ffdfb5f62791ef0ce",
+    "url": "/react-robot-app/index.html"
+  },
+  {
+    "revision": "755403bae5ba9fd430af",
+    "url": "/react-robot-app/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "b5ed2fed5756a6a743ac",
+    "url": "/react-robot-app/static/css/main.c00a24c9.chunk.css"
+  },
+  {
+    "revision": "755403bae5ba9fd430af",
+    "url": "/react-robot-app/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-robot-app/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5ed2fed5756a6a743ac",
+    "url": "/react-robot-app/static/js/main.4b7abbf7.chunk.js"
+  },
+  {
+    "revision": "f9cd74033b9d3090d089",
+    "url": "/react-robot-app/static/js/runtime-main.b376ceac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robot-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
